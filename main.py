696b756b083a5d6cb5b689ca93ff73a27f1a66b5
stylix333 <satyaevan01@gmail.com>
2026-06-10 01:44:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello Kizuna")
+# main.py
+import tkinter as tk
+from gui import KizunaApp
+
+def main():
+    root = tk.Tk()
+    app = KizunaApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
